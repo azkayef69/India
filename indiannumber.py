7a0639a029c3b34e979b3a6cb9cd8ba84a5f475a
azkayef69 <103218602+azkayef69@users.noreply.github.com>
2022-10-19 19:03:51 +0600

    Add files via upload

---
+++ indiannumber.py
@@ -0,0 +1,13 @@
+
+
+import os,sys,time,random
+op=open('op.txt' , 'r').read ().split()
+c=0
+for o in op:
+	  c+=1
+	  cc='+91' +o
+	  pc=random.randint(100000,999999)
+	  number=cc+str(pc)
+	  print(c,number,len(number))
+	  open('numbers-generated.txt' , 'a').write(f'{c}]     '+number+'\n')
+      time.sleep(0.1)
